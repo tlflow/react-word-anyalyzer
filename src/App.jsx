447a92ac78edff6a95a9e49c06ebc7e0bd8ac872
Terence Flowers <tlflow33@gmail.com>
2024-06-13 21:24:52 -0400

    add a few more checks: (#1)
    - check for no of sentences
    - X content limit check

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useState, useRef, useEffect } from 'react';
 import { pronouns } from './pronouns';
+import { settings } from './settings';
 import './App.css';
 
 function App() {
   const [charLength, setCharLength] = useState(0);
   const [wordLength, setWordLength] = useState(0);
   const [pronounsLength, setPronounsLength] = useState(0);
+  const [numofSentences, setNumofSentences] = useState(0);
   const [longestWord, setLongestWord] = useState('');
   const [readTime, setReadTime] = useState('');
+  const [xLimitCheck, setXLimitCheck] = useState(false);
 
   const inputRef = useRef('');
+
+  const { 
+    AVG_READING_TIME,
+    X_LIMIT,
+   } = settings;
 
   function getCharLength(str) {
     setCharLength(str.length);
@@ -32,7 +40,9 @@ function App() {
     const locatedPronouns = norm_str.match(
       new RegExp(`\\b(${norm_prn.join('|')})\\b`, 'g')
     );
-    setPronounsLength(locatedPronouns.length);
+    locatedPronouns 
+      ? setPronounsLength(locatedPronouns.length)
+      : ""
   }
 
   function getLongestWord(str) {
@@ -48,8 +58,8 @@ function App() {
 
   function getReadTime(str) {
     const words = str.match(/\S+/g);
-    /* avg reading time is 185 words per minute */
-    const avgReadingTime = 185 / 60;
+    
+    const avgReadingTime = AVG_READING_TIME / 60;
 
     if (words.length !== 0) {
       const timeInSeconds = words.length / avgReadingTime;
@@ -62,12 +72,25 @@ function App() {
     }
   }
 
+  function getNumberofSentences(str) {
+    const sentences = str.match(/\w[.?!](\s|$)/g);
+    sentences 
+      ? setNumofSentences(sentences.length)
+      : 0
+  }
+
+  function isOverXLimit() {    
+    setXLimitCheck(charLength >= X_LIMIT)    
+  }
+
   function updateSpecs(inputValue) {
     getCharLength(inputValue);
     getWordLength(inputValue);
     getLongestWord(inputValue);
-    // getPronounsLength(inputValue);
+    getPronounsLength(inputValue);
+    getNumberofSentences(inputValue);
     getReadTime(inputValue);
+    isOverXLimit();
   }
 
   /* focus on input field on page render */
@@ -82,7 +105,9 @@ function App() {
       <div>No. of Words {wordLength}</div>
       <div>Longest Word {longestWord}</div>
       <div>No. of Pronouns {pronounsLength}</div>
+      <div>No. of Sentences {numofSentences}</div>
       <div>Time to Read Msg {readTime}</div>
+      <div>Over X Limit {xLimitCheck ? 'yes' : 'no'}</div>
       <div>
         <label htmlFor="">
           <textarea
